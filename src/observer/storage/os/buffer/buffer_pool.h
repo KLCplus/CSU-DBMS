fcbf36fd9e9d9b6d5c46cd2691a548b9350e63fb
KLC <991407561@qq.com>
2026-09-09 21:18:06 +0800

    refactor: add organized OS storage facade

---
+++ src/observer/storage/os/buffer/buffer_pool.h
@@ -0,0 +1,6 @@
+#pragma once
+
+// OS layer: the page-cache service used by Record, Table, Index and WAL code.
+#include "storage/buffer/disk_buffer_pool.h"
+#include "storage/buffer/double_write_buffer.h"
+
---
+++ src/observer/storage/os/record/record_page.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// Database-to-OS bridge: logical records/RIDs mapped onto managed pages.
+#include "storage/record/record.h"
+#include "storage/record/record_manager.h"
+#include "storage/record/heap_record_scanner.h"
+
